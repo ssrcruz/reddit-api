b7d7f4488bd0029ac818f09f7b38923c4948a0d6
Ruben <ssrcruz@gmail.com>
2017-04-27 11:35:42 -0400

    Initial Commit

---
+++ app/javascript/containers/Root.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+
+export default class Root extends Component {
+  render() {
+    return (
+      <div>Something</div>
+    )
+  }
+}
